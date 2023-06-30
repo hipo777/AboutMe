47a4f168059190f07d3923535561761a19c704c6
hipolito <huaylla7@hotmail.com>
2023-06-29 20:20:09 -0400

    Activity main funcionando

--- app/src/main/java/org/matematicasfacil/aboutme/MainActivity.java
+++ app/src/main/java/org/matematicasfacil/aboutme/MainActivity.java
@@ -2,7 +2,11 @@ package org.matematicasfacil.aboutme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import org.matematicasfacil.aboutme.databinding.ActivityMainBinding;
 
@@ -12,6 +16,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.floatingCompartir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ContacActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
